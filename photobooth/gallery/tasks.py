8851ba2ad3cc4473366dc5cc22281dac7dc64637
Rolf Erik Lekang <me@rolflekang.com>
2015-11-21 16:35:42 +0100

    Set resolution lower

--- photobooth/gallery/tasks.py
+++ photobooth/gallery/tasks.py
@@ -13,7 +13,7 @@ def take_photo():
 
     logger.info('Starting photo capture')
     with picamera.PiCamera() as camera:
-        camera.resolution = (2592, 1944)
+        camera.resolution = (1440, 1080)
         camera.start_preview()
         time.sleep(1)
         camera.capture('foo.jpg')
