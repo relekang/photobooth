1cb6996efeca7550e03da9f5e11ac9a0429b2be5
Rolf Erik Lekang <me@rolflekang.com>
2015-11-21 14:12:50 +0100

    Fix thumbnails

--- photobooth/gallery/models.py
+++ photobooth/gallery/models.py
@@ -15,7 +15,3 @@ class Photo(TimeStampModel):
     @property
     def thumbnail(self):
         return get_thumbnail(os.path.join(settings.MEDIA_ROOT, self.file.name), '300')
-
-    @property
-    def thumbnail_url(self):
-        return self.thumbnail.url
--- photobooth/gallery/serializers.py
+++ photobooth/gallery/serializers.py
@@ -3,7 +3,19 @@ from rest_framework import serializers
 from .models import Photo
 
 
+class ThumbnailSerializer(serializers.BaseSerializer):
+    def to_representation(self, obj):
+        request = self.context['request']
+        return 'http{s}://{host}{path}'.format(
+            s='s' if request.is_secure() else '',
+            host=request.get_host(),
+            path=obj.url
+        )
+
+
 class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+    thumbnail = ThumbnailSerializer(read_only=True)
+
     class Meta:
         model = Photo
         fields = (
@@ -11,5 +23,5 @@ class PhotoSerializer(serializers.HyperlinkedModelSerializer):
             'file',
             'created_at',
             'updated_at',
-            'thumbnail_url',
+            'thumbnail',
         )
