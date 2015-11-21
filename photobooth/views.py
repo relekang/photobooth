5a27f4163cee0582904a9a1d3c23dbf1c9d7b878
Rolf Erik Lekang <me@rolflekang.com>
2015-11-21 18:07:41 +0100

    Add ensure csrf cookie in landing page

--- photobooth/views.py
+++ photobooth/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 
 
 class LandingPage(TemplateView):
     template_name = 'index.html'
+
+    @method_decorator(ensure_csrf_cookie)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class Remote(LoginRequiredMixin, TemplateView):
