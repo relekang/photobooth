f1a1a2b92c1136c8659be92056c4af2bf973a97b
Rolf Erik Lekang <me@rolflekang.com>
2015-11-21 16:29:30 +0100

    Add logging in take_photo task

--- photobooth/gallery/tasks.py
+++ photobooth/gallery/tasks.py
@@ -1,20 +1,31 @@
 import time
+import logging
 
 import requests
 from celery import shared_task
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def take_photo():
     import picamera
 
+    logger.info('Starting photo capture')
     with picamera.PiCamera() as camera:
         camera.resolution = (2592, 1944)
         camera.start_preview()
         time.sleep(1)
         camera.capture('foo.jpg')
         camera.stop_preview()
+        logger.info('Saved image to disk, starting upload')
         files = {'file': open('foo.jpg', 'rb')}
         response = requests.post("http://booth.lkng.me/api/photos/", files=files)
-        print(response.status_code)
-        print(response.text)
+
+        if response.status_code == 200 or response.status_code == 201:
+            logger.info('Upload complete')
+        else:
+            logger.error(
+                'Could not upload image (status code from server: {}'.format(response.status_code),
+                extra={'response': response}
+            )
